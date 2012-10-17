2a5da3ac8809482e3b8356d1cb959006627df380
Abhishek Das <das.abhshk@gmail.com>
2012-10-18 00:04:32 +0530

    Add social icons Still need to add hyperlinks

--- index.php
+++ index.php
@@ -13,137 +13,302 @@
       <div id="container">
         <div class="box photo col4">
           <img src="pics/aarti.jpg" alt="Aarti" />
-          <div class="img-title">&nbsp;&nbsp;Aarti Dwivedi</div>
+          <div class="img-bar">
+            <span class="img-title">Aarti Dwivedi</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       
         <div class="box photo col4">
           <img src="pics/abhay.jpg" alt="Abhay" />
-          <div class="img-title">&nbsp;&nbsp;Abhay Bir Singh Rana</div>
+          <div class="img-bar">
+            <span class="img-title">Abhay Bir Singh Rana</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       
         <div class="box photo col4">
           <img src="pics/abhishek.jpg" alt="Abhishek" />
-          <div class="img-title">&nbsp;&nbsp;Abhishek Das</div>
+          <div class="img-bar">
+            <span class="img-title">Abhishek Das</span>
+            <span class="img-icons">
+              <a href="http://stackoverflow.com/users/1257954/abhshkdz"><img src="assets/so.png"></a>
+              <a href="https://github.com/abhshkdz"><img src="assets/gh.png"></a>
+              <a href="https://plus.google.com/u/0/100610196494221761914/"><img src="assets/gplus.png"></a>
+              <a href="https://www.facebook.com/AbhshkDz"><img src="assets/fb.png"></a>
+              <a href="https://twitter.com/"><img src="assets/twitter.png"></a>
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/aditya.jpg" alt="Aditya" />
-          <div class="img-title">&nbsp;&nbsp;Aditya Garg</div>
+          <div class="img-bar">
+            <span class="img-title">Aditya Garg</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/apoorva.jpg" alt="Apoorva" />
-          <div class="img-title">&nbsp;&nbsp;Apoorva Mudgal</div>
+          <div class="img-bar">
+            <span class="img-title">Apoorva Mudgal</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/arun.jpg" alt="Arun" />
-          <div class="img-title">&nbsp;&nbsp;Arun Chetty</div>
+          <div class="img-bar">
+            <span class="img-title">Arun Chetty</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       
         <div class="box photo col4">
           <img src="pics/ashwini.jpg" alt="Ashwini" />
-          <div class="img-title">&nbsp;&nbsp;Ashwini Khare</div>
+          <div class="img-bar">
+            <span class="img-title">Ashwini Khare</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/divyanshu.jpg" alt="Divyanshu" />
-          <div class="img-title">&nbsp;&nbsp;Divyanshu Upreti</div>
+          <div class="img-bar">
+            <span class="img-title">Divyanshu Upreti</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/durgesh.jpg" alt="Durgesh" />
-          <div class="img-title">&nbsp;&nbsp;Durgesh Suthar</div>
+          <div class="img-bar">
+            <span class="img-title">Durgesh Suthar</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/giri.jpg" alt="Giri" />
-          <div class="img-title">&nbsp;&nbsp;Giridaran Manivannan</div>
+          <div class="img-bar">
+            <span class="img-title">Giridaran Manivannan</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       
         <div class="box photo col4">
           <img src="pics/harshil.jpg" alt="Harshil" />
-          <div class="img-title">&nbsp;&nbsp;Harshil Mathur</div>
+          <div class="img-bar">
+            <span class="img-title">Harshil Mathur</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/jayant.jpg" alt="Jayant" />
-          <div class="img-title">&nbsp;&nbsp;Jayant Jain</div>
+          <div class="img-bar">
+            <span class="img-title">Jayant Jain</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/neeraj.jpg" alt="Neeraj" />
-          <div class="img-title">&nbsp;&nbsp;Neeraj Gangwar</div>
+          <div class="img-bar">
+            <span class="img-title">Neeraj Gangwar</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       
         <div class="box photo col4">
           <img src="pics/nisha.jpg" alt="Nisha" />
-          <div class="img-title">&nbsp;&nbsp;Nisha Chandramoorthy</div>
+          <div class="img-bar">
+            <span class="img-title">Nisha Chandramoorthy</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/nitish.jpg" alt="Nitish" />
-          <div class="img-title">&nbsp;&nbsp;Nitish Sharma</div>
+          <div class="img-bar">
+            <span class="img-title">Nitish Sharma</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/obuli.jpg" alt="Obuli" />
-          <div class="img-title">&nbsp;&nbsp;Obuli Vignesh</div>
+          <div class="img-bar">
+            <span class="img-title">Obuli Vignesh</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       
         <div class="box photo col4">
           <img src="pics/ravi.jpg" alt="Ravi" />
-          <div class="img-title">&nbsp;&nbsp;Ravi Kishore</div>
+          <div class="img-bar">
+            <span class="img-title">Ravi Kishore</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/richa.jpg" alt="Richa" />
-          <div class="img-title">&nbsp;&nbsp;Richa Jain</div>
+          <div class="img-bar">
+            <span class="img-title">Richa Jain</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/rushil.jpg" alt="Rushil" />
-          <div class="img-title">&nbsp;&nbsp;Rushil Nagda</div>
+          <div class="img-bar">
+            <span class="img-title">Rushil Nagda</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       
         <div class="box photo col4">
           <img src="pics/sagar.jpg" alt="Sagar" />
-          <div class="img-title">&nbsp;&nbsp;Sagar Saxena</div>
+          <div class="img-bar">
+            <span class="img-title">Sagar Saxena</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/saket.jpg" alt="Saket" />
-          <div class="img-title">&nbsp;&nbsp;Saket Sarupria</div>
+          <div class="img-bar">
+            <span class="img-title">Saket Sarupria</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       
         <div class="box photo col4">
           <img src="pics/sethu.jpg" alt="Sethu" />
-          <div class="img-title">&nbsp;&nbsp;Sethu Nachiappan</div>
+          <div class="img-bar">
+            <span class="img-title">Sethu Nachiappan</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/shagun.jpg" alt="Shagun" />
-          <div class="img-title">&nbsp;&nbsp;Shagun Sodhani</div>
+          <div class="img-bar">
+            <span class="img-title">Shagun Sodhani</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/shashank.jpg" alt="Shashank" />
-          <div class="img-title">&nbsp;&nbsp;Shashank Mehta</div>
+          <div class="img-bar">
+            <span class="img-title">Shashank Mehta</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/shikhar.jpg" alt="Shikhar" />
-          <div class="img-title">&nbsp;&nbsp;Shikhar Shrivastava</div>
+          <div class="img-bar">
+            <span class="img-title">Shikhar Shrivastava</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/shivam.jpg" alt="Shivam" />
-          <div class="img-title">&nbsp;&nbsp;Shivam Gupta</div>
+          <div class="img-bar">
+            <span class="img-title">Shivam Gupta</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
 
         <div class="box photo col4">
           <img src="pics/shreya.jpg" alt="Shreya" />
-          <div class="img-title">&nbsp;&nbsp;Shreya Bhardwaj</div>
+          <div class="img-bar">
+            <span class="img-title">Shreya Bhardwaj</span>
+            <span class="img-icons">
+              <img src="assets/fb.png">
+              <img src="assets/twitter.png">
+            </span>
+          </div>
         </div>
       </div>
     </div>

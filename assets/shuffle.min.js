e91cdfc48e5ac7ff5b48cc46e949e29c76ad9afd
alphadose <anish.mukherjee1996@gmail.com>
2017-02-09 02:46:29 +0530

    random

---
+++ assets/shuffle.min.js
@@ -0,0 +1,1 @@
+(function(d){d.fn.shuffle=function(c){c=[];return this.each(function(){c.push(d(this).clone(true))}).each(function(a,b){d(b).replaceWith(c[a=Math.floor(Math.random()*c.length)]);c.splice(a,1)})};d.shuffle=function(a){return d(a).shuffle()}})(jQuery);

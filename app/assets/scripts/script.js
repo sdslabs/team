c1077ec7d466cb041c8c1e14b89fc02ab2ad68f5
kritisingh1 <kritisingh1.ks@gmail.com>
2017-07-27 22:37:38 +0530

    enabled navigation

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -1,8 +1,7 @@
 jQuery(document).ready(function($){
+	
 	var slide = 0;
-
 	var routes ={
-		/*"/mainpage": "mainpage",*/
 		"designerpage": "designerpage",
 		"/developerpage": "developerpage",
 		"alumnilocationpage": "alumnilocationpage",
@@ -20,11 +19,13 @@ jQuery(document).ready(function($){
 
 	var designerVennButton = $('#designer-venn-button');
 	var developerVennButton = $('#developer-venn-button');
-
 	var navUpButton = $('#nav-up-button');
 	var navDownButton = $('#nav-down-button');
 	var crossButton = $('#cross-button');
 	var pageContainer = $('.page-container');
+	var designerSideBar = $('#designer-side-bar');
+	var developerSideBar = $('#developer-side-bar');
+	var svgIcon = $('#svg-icon');
 
 	var pages = pageContainer.children('.page');
 
@@ -46,8 +47,33 @@ jQuery(document).ready(function($){
 		}
 	}
 
+	function display(index) {
+		$(pages[index]).addClass('selected');
+		$(pages[index-1]).removeClass('selected');
+		$(pages[index-1]).addClass('hidden');
+		$(pages[index-2]).removeClass('selected');
+		$(pages[index-2]).addClass('hidden');
+		$(pages[index-3]).removeClass('selected');
+		$(pages[index-3]).addClass('hidden');
+		$(pages[index-4]).removeClass('selected');
+		$(pages[index-4]).addClass('hidden');
+		$(pages[index+1]).removeClass('selected');
+		$(pages[index+1]).addClass('hidden');
+		$(pages[index+2]).removeClass('selected');
+		$(pages[index+2]).addClass('hidden');
+		$(pages[index+3]).removeClass('selected');
+		$(pages[index+3]).addClass('hidden');
+		$(pages[index+4]).removeClass('selected');
+		$(pages[index+4]).addClass('hidden');
+		
+	}
+
 	window.onpopstate =function(event) {
 		matchUrl(document.location.href);
+	}
+
+	function firstPage(){
+		$(pages[0]).addClass('selected');
 	}
 
 	function updateSlide(prevSlideIndex, nextSlideIndex) {
@@ -107,7 +133,7 @@ jQuery(document).ready(function($){
 			crossButton.hide();
 		}
 
-		else if(slide == 1 || slide ==2) {
+		else if(slide == 1 || slide == 2) {
 			crossButton.show();
 			navDownButton.hide();
 			navUpButton.hide();
@@ -136,19 +162,47 @@ jQuery(document).ready(function($){
 	var navClick = {
 		down : function() {
 			matchUrl(pageUrls[3]);
+			display(3);
 		},
 		up : function() {
 			matchUrl(pageUrls[0]);
+			display(0);
 		},
 		cross : function() {
 			matchUrl(pageUrls[0]);
+			display(0);
+		},
+		ds : function() {
+			matchUrl(pageUrls[1]);
+			display(1);
+		},	
+		dv : function() {
+			matchUrl(pageUrls[2]);
+			display(2);
+		},
+		dsbar : function() {
+			matchUrl(pageUrls[1]);
+			display(1);
+		},
+		dvbar : function() {
+			matchUrl(pageUrls[2]);
+			display(2);
+		},
+		numsvg : function() {
+			matchUrl(pageUrls[4]);
+			display(4);
 		}
+
 	}
 
 	navUpButton.on('click', navClick.up);
 	navDownButton.on('click', navClick.down);
 	crossButton.on('click', navClick.cross);
+	designerVennButton.on('click', navClick.ds);
+	developerVennButton.on('click', navClick.dv);
+	designerSideBar.on('click', navClick.dsbar);
+	developerSideBar.on('click', navClick.dvbar);
+	svgIcon.on('click', navClick.numsvg);
 
-	
-
-})
+	firstPage();
+});
